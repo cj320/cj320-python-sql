1356b82233ad55009d3eb6e646631e87726d3ae3
Carter James <carterjames6316@gmail.com>
2021-12-28 18:09:24 -0500

    Modified models.py

--- infotech/src/models.py
+++ infotech/src/models.py
@@ -17,6 +17,56 @@ class Encoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj,Decimal): return float(obj)
 
+# Create the likes table which consists of a many to many relationship.
+association_table = db.Table(
+    'employee_to_contracts',
+    db.Column(
+        'employee_id', db.Integer,
+        db.ForeignKey('employees.id'),
+        primary_key=True
+    ),
+    db.Column(
+        'contract_id', db.Integer,
+        db.ForeignKey('contracts.id'),
+        primary_key=True
+    ),
+    db.Column(
+        'labor_id', db.Integer,
+        db.ForeignKey('labor_categories.id'),
+        primary_key=True
+    )
+)
+class Employee(db.Model):
+    __tablename__ = "employees"
+        # Set autoincrement to user the SERIAL datatype
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(20),nullable=False)
+    last_name = db.Column(db.String(20),nullable=False)
+    email_address = db.Column(db.String(50),unique=True,nullable=False)
+    labor_id = db.Column(db.Integer,db.ForeignKey('labor_categories.id'), nullable=False)
+    contract_id = db.Column(db.Integer, db.ForeignKey('contracts.id'), nullable=False)
+    contract = db.relationship("Contract", foreign_keys=[contract_id])
+    labor_category = db.relationship("Labor", foreign_keys=[labor_id])
+
+    def __init__(self,first_name:str,last_name:str,email_address:str,labor_id:int,contract_id:int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email_address = email_address
+        self.labor_id = labor_id
+        self.contract_id = contract_id
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email_address': self.email_address,
+            'labor_id': self.labor_id,
+            'contract_id': self.contract_id
+        }
+
+
+
 # Create the User class as a subclass of SQLAlchemy's db.Model
 class Labor(db.Model):
     __tablename__ = "labor_categories"
@@ -24,8 +74,7 @@ class Labor(db.Model):
     labor_code = db.Column(db.String(5), unique=True,nullable=False)
     labor_category = db.Column(db.String(100),unique=True,nullable=False)
     salary = db.Column(db.Numeric(10,2), nullable=False)
-    #employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'),unique=True)
-    worker = db.relationship('Employee', backref='employee')
+    employee_labor = db.relationship('Employee', backref='employee', lazy='dynamic')
 
 
     def __init__(self,labor_code:str,labor_category:str,salary:float):
@@ -47,8 +96,7 @@ class Contract(db.Model):
     contract_name = db.Column(db.String(50),unique=True,nullable=False)
     customer_name = db.Column(db.String(50),nullable=False)
     open_reqs = db.Column(db.Integer, nullable=True)
-    #labor = db.relationship('Labor', backref='labor')
-    worker_id = db.relationship('Employee', backref='employee')
+    workers = db.relationship('Employee', foreign_keys=[Employee.contract_id], backref='workers')
 
     def __init__(self,contract_name:str,customer_name:str,open_reqs:int):
         self.contract_name = contract_name
@@ -62,49 +110,4 @@ class Contract(db.Model):
             'customer_name': self.customer_name,
             'open_reqs': self.open_reqs
         }
-# Create the likes table which consists of a many to many relationship.
-association_table = db.Table(
-    'employee_to_contracts',
-    db.Column(
-        'employee_id', db.Integer,
-        db.ForeignKey('employees.id'),
-        primary_key=True
-    ),
-    db.Column(
-        'contract_id', db.Integer,
-        db.ForeignKey('contracts.id'),
-        primary_key=True
-    ),
-    db.Column(
-        'labor_id', db.Integer,
-        db.ForeignKey('labor_categories.id'),
-        primary_key=True
-    )
-)
 
-class Employee(db.Model):
-    __tablename__ = "employees"
-        # Set autoincrement to user the SERIAL datatype
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    first_name = db.Column(db.String(20),nullable=False)
-    last_name = db.Column(db.String(20),nullable=False)
-    email_address = db.Column(db.String(50),unique=True,nullable=False)
-    labor_id = db.Column(db.Integer,db.ForeignKey('labor_categories.id'), nullable=False)
-    contract_id = db.Column(db.Integer, db.ForeignKey('contracts.id'), nullable=False)
-
-    def __init__(self,first_name:str,last_name:str,email_address:str,labor_id:int,contract_id:int):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email_address = email_address
-        self.labor_id = labor_id
-        self.contract_id = contract_id
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email_address': self.email_address,
-            'labor_id': self.labor_id,
-            'contract_id': self.contract_id
-        }
